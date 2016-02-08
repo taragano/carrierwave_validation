ff99f3ccff43666dc96257855a59b892351bc2a2
Itay Taragano <itay.taragano@cloudinary.com>
2016-02-08 12:41:31 +0200

    Clear cached images after save.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 	validate :max_dimensions
+	after_save :clear_cache
 
   def max_dimensions
   	max_width = 1000
@@ -8,5 +9,9 @@ class Post < ActiveRecord::Base
     if width > max_width
       errors.add(:image, ": You cannot upload an image larger than #{max_width} pixels")
     end
+  end
+
+  def clear_cache
+		CarrierWave.clean_cached_files! 0
   end
 end
